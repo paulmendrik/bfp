db2a0013a28344887d045f506434929876be7e3d
Paul Mendrik <paul@imouse.net>
2021-05-05 15:32:53 +0200

    Particles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head';
-import {Column, Content, Navbar} from "rbx";
+import Image from 'next/image';
+import {Column, Content, Navbar, Footer} from "rbx";
 import Forward from "../components/Forward";
 import React from "react";
+import Particles from 'react-particles-js';
+
 
 
 export default function Home() {
@@ -9,26 +12,31 @@ export default function Home() {
       <main>
       <Head>
           <title>Bill Funk Photography Intro</title>
-          <link rel="manifest" href="/manifest.json" />
+          <link rel="manifest" href="/manifest.json" />  
       </Head>
-          <Column.Group centered>
-              <Column size="two-thirds page">
-                  <Navbar>
-                      <Navbar.Brand><Navbar.Item href="/"><h1 className="logo">Bill Funk</h1></Navbar.Item></Navbar.Brand>
-                      <Navbar.Menu><Navbar.Segment align="end"></Navbar.Segment></Navbar.Menu>
-                  </Navbar>
-                  <div class="blobs">
-                <div class="blob"></div>
-                <div class="blob"></div>
-                <div class="blob"></div>
-                    </div>
-                  <Content className="intro">
-                     <Forward/>
-
-                          <p>&copy; Bill Funk Photography. All rights reserved.</p>
-                  </Content>
-              </Column>
-          </Column.Group>
+        <Column.Group centered>
+        <Column size="two-thirds page">
+        <Navbar>
+        <Navbar.Brand><Navbar.Item href="/"><h1 className="logo">Bill Funk</h1></Navbar.Item></Navbar.Brand>
+        <Navbar.Menu><Navbar.Segment align="end"></Navbar.Segment></Navbar.Menu>
+        </Navbar>
+        <Column.Group centered>
+        <Column className="main" size="full">
+        <Particles />
+        </Column>
+        </Column.Group>
+        <Column.Group>
+        <Column size="full">
+        <Footer>
+        <Content textAlign="centered">
+            <Forward />
+        <p className="is-size-7">&copy; Bill Funk Photography. All rights reserved.</p>
+        </Content>
+        </Footer>  
+        </Column>
+        </Column.Group>
+        </Column>
+        </Column.Group> 
       </main>
 
 
